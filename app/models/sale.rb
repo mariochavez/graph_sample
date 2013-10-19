9c6b75e475832232d4855cff3b14b59ad1293e6f
Mario A Chavez <mario.chavez@gmail.com>
2013-10-19 04:05:27 +0000

    Update queries

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,18 +1,20 @@
 class Sale < ActiveRecord::Base
+  scope :by_time, ->(start, finish) { where(created_at: (start..finish)) }
+
   class << self
     def by_month_totals
-      group_by_month(:created_at).sum(:total)
+      group_by_month(:created_at).order('MAX(created_at) DESC').sum(:total)
     end
 
     def by_day_totals
-      where('created_at >= ?', Time.now.beginning_of_day)
-      .where('created_at <= ?', Time.now.end_of_day)
+      by_time(Time.now.beginning_of_day, Time.now.end_of_day)
+      .order('MAX(created_at) DESC')
       .group_by_minute(:created_at).sum(:total)
     end
 
     def by_week_totals
-      where('created_at >= ?', Time.now.beginning_of_month)
-      .where('created_at <= ?', Time.now.end_of_month)
+      by_time(Time.now.beginning_of_month, Time.now.end_of_month)
+      .order('MAX(created_at) DESC')
       .group_by_week(:created_at).sum(:total)
     end
   end
